a9d192a385b287f495a609c795364ffedee7f747
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-01-17 10:27:06 -0300

    Creación de data: categories and products, para que seeder cuente con productos y categorias

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from '../config/typeorm.config';
 import { SeederService } from './seeder.service';
+import { Product } from '../products/entities/product.entity';
+import { Category } from '../categories/entities/category.entity';
 
 
 @Module({
@@ -14,6 +16,7 @@ import { SeederService } from './seeder.service';
           useFactory: typeOrmConfig,
           inject: [ConfigService]
         }),
+        TypeOrmModule.forFeature([Product, Category])
     ],
     providers: [SeederService]
 })
--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from '../categories/entities/category.entity';
+import { Repository } from 'typeorm';
+import { Product } from '../products/entities/product.entity';
+import { categories } from './data/categories';
+import { products } from './data/products';
 
 @Injectable()
 export class SeederService {
+    constructor(
+        @InjectRepository(Category) private readonly categoryRepository: Repository<Category>,
+        @InjectRepository(Product) private readonly productRepository: Repository<Product>
+    ) {}
 
     async seed() {
-        console.log('Desde seed en seeder.service.ts')
+        await this.categoryRepository.save(categories)
+
+        for await (const seedProduct of products) {
+            const category = await this.categoryRepository.findOneBy({ id: seedProduct.categoryId })
+            const product = new Product()
+            product.name = seedProduct.name
+            product.image = seedProduct.image
+            product.price = seedProduct.price
+            product.inventory = seedProduct.inventory
+            product.category = category
+
+            await this.productRepository.save(product)
+        }
     }
 
 }
