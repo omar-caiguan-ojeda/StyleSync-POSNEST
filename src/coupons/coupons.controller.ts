36d94fbce5e6cef544ee7891184cd28ac18b6a64
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-01-17 07:01:16 -0300

    Asignando cupones de descuento: controller

--- src/coupons/coupons.controller.ts
+++ src/coupons/coupons.controller.ts
@@ -3,6 +3,7 @@ import { CouponsService } from './coupons.service';
 import { CreateCouponDto } from './dto/create-coupon.dto';
 import { UpdateCouponDto } from './dto/update-coupon.dto';
 import { IdValidationPipe } from 'src/common/pipes/id-validation/id-validation.pipe';
+import { ApplyCouponDto } from './dto/apply-coupon.dto';
 
 @Controller('coupons')
 export class CouponsController {
@@ -34,5 +35,10 @@ export class CouponsController {
   @Delete(':id')
   remove(@Param('id', IdValidationPipe) id: string) {
     return this.couponsService.remove(+id);
+  }
+
+  @Post('/apply-coupon')
+  applyCoupon(@Body() applyCouponDto: ApplyCouponDto) {
+    console.log(applyCouponDto)
   }
 }
